404636e8a32c95c8f01183f547bd504ec4f1507f
Oliver Christensen <orc13a@live.dk>
2022-01-05 13:09:36 +0100

    items array

---
+++ js/items.js
@@ -0,0 +1,122 @@
+const items = [
+    {
+        item: 'kort',
+        weight: 90,
+        price: 150
+    },
+    {
+        item: 'kompas',
+        weight: 130,
+        price: 35
+    },
+    {
+        item: 'vand',
+        weight: 1530,
+        price: 200
+    },
+    {
+        item: 'sandwich',
+        weight: 500,
+        price: 160
+    },
+    {
+        item: 'sukker',
+        weight: 150,
+        price: 60
+    },
+    {
+        item: 'dåsemad',
+        weight: 680,
+        price: 45
+    },
+    {
+        item: 'banan',
+        weight: 270,
+        price: 60
+    },
+    {
+        item: 'æble',
+        weight: 390,
+        price: 40
+    },
+    {
+        item: 'ost',
+        weight: 230,
+        price: 30
+    },
+    {
+        item: 'øl',
+        weight: 520,
+        price: 10
+    },
+    {
+        item: 'solcreme',
+        weight: 110,
+        price: 70
+    },
+    {
+        item: 'kamera',
+        weight: 320,
+        price: 30
+    },
+    {
+        item: 'T-shirt',
+        weight: 240,
+        price: 15
+    },
+    {
+        item: 'bukser',
+        weight: 480,
+        price: 10
+    },
+    {
+        item: 'paraply',
+        weight: 730,
+        price: 40
+    },
+    {
+        item: 'vandtætte bukser',
+        weight: 420,
+        price: 70
+    },
+    {
+        item: 'vandtæt overtøj',
+        weight: 430,
+        price: 75
+    },
+    {
+        item: 'pung',
+        weight: 220,
+        price: 80
+    },
+    {
+        item: 'solbriller',
+        weight: 70,
+        price: 20
+    },
+    {
+        item: 'håndklæde',
+        weight: 180,
+        price: 12
+    },
+    {
+        item: 'sokker',
+        weight: 40,
+        price: 50
+    },
+    {
+        item: 'bog',
+        weight: 300,
+        price: 10
+    },
+    {
+        item: 'notesbog',
+        weight: 900,
+        price: 1
+    },
+    {
+        item: 'telt',
+        weight: 2000,
+        price: 150
+    }
+] 
