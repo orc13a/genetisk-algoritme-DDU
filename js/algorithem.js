ec32cc4380c778b5958d0416a8b3ad932b701c33
Oliver Christensen <orc13a@live.dk>
2022-01-08 23:19:39 +0100

    create start

--- js/algorithem.js
+++ js/algorithem.js
@@ -1,4 +1,6 @@
 // Trin 1, lav tilfældige start tasker
 for (let i = 1; i <= 2000; i++) {
-    
+    createFirstGeneration();
 }
+
+console.log(allBps);
--- js/main.js
+++ js/main.js
@@ -2,3 +2,25 @@
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function getItem(index) {
+    return items[index];
+}
+
+// All backpacks
+let allBps = [];  
+
+function createFirstGeneration() {
+    const backSize = random(10, 23);
+    let bp = [];
+
+    while (bp.length <= backSize) {
+        let randomItem = getItem(random(0, 23));
+        while (bp.includes(randomItem) === true) {
+            randomItem = getItem(random(0, 23));
+        }
+        bp.push(randomItem);
+    }
+
+    allBps.push(bp);
+}
