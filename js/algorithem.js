be7dd314aa6eed08f9010c80d651aa431e1021c7
Oliver Christensen <orc13a@live.dk>
2022-01-08 23:05:10 +0100

    v2 init

--- js/algorithem.js
+++ js/algorithem.js
@@ -1,24 +1,4 @@
-let generation = 1;
-let lastBestpackGeneration = 1;
-let bestBackpack = {
-    totalWeight: 0,
-    totalValue: 0
-};
-let run = true;
-
 // Trin 1, lav tilfældige start tasker
-for (let i = 0; i < 500001; i++) {
-    generatePossibleBackpacks();
+for (let i = 1; i <= 2000; i++) {
+    
 }
-
-while (run) {
-    // Trin 2
-    calFitnessForAll();
-
-    // Trin 3
-    reproduction();
-}
-
-// console.log(allBackpacks);
-console.log(bestBackpack);
-// console.log(nextAllBackpacks);
